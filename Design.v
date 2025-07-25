a8ae1b6dc4e290d07cc14a09b3d91d7f7c0dca19
katuri yeshwanth <katuri.yeswanth@gmail.com>
2025-07-25 17:34:58 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -6,3 +6,14 @@ f1 <= a&b;
 always @(negedge clk2)
 f2 <= a^c;
 endmodule
+
+single clock multiple edge
+
+module clk(clk,a,b,c,f1,f2);
+input clk,a,b,c;
+output reg f1,f2;
+always @(posedge clk)
+f1 <= a&b;
+always @(negedge clk)
+f2 <= a^c;
+endmodule
